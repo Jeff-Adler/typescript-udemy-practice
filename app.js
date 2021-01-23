fb733079efee3a7e545f0987bf82d744e70da5b1
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-23 16:34:13 -0500

    type aliases

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var combine = function (input1, input2) {
+var combine = function (input1, input2, resultConversion) {
+    if (resultConversion === "as-number" && (isNaN(+input1) || isNaN(+input2))) {
+        console.log("hit RETURN condition");
+        return;
+    }
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" &&
+        typeof input2 === "number" &&
+        resultConversion !== "as-text") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 };
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-text");
 console.log(combinedAges);
-var combinedNames = combine("emily", "sarah");
+var combinedNames = combine("emily", "sarah", "as-number");
 console.log(combinedNames);
+var combinedNums = combine("5", "3", "as-number");
+console.log(combinedNums);
--- app.ts
+++ app.ts
@@ -1,15 +1,34 @@
-const combine = (input1: number | string, input2: number | string) => {
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
+const combine = (
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) => {
+  if (resultConversion === "as-number" && (isNaN(+input1) || isNaN(+input2))) {
+    console.log("hit RETURN condition");
+    return;
+  }
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" &&
+      typeof input2 === "number" &&
+      resultConversion !== "as-text") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 };
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-text");
 console.log(combinedAges);
 
-const combinedNames = combine("emily", "sarah");
+const combinedNames = combine("emily", "sarah", "as-number");
 console.log(combinedNames);
+
+const combinedNums = combine("5", "3", "as-number");
+console.log(combinedNums);
