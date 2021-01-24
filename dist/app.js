1aa129946306efbbb74ec12c94c5da5feedf38d0
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-24 12:10:02 -0500

    inheritance

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,49 @@
 "use strict";
 class Department {
-    constructor(n) {
-        this.name = n;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
     }
     describe() {
-        console.log(`Department: ${this.name}`);
+        console.log(`Department ${this.id}: ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-const accounting = new Department("Accounting");
-accounting.describe();
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "IT");
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "IT");
+        this.reports = reports;
+    }
+    addReport(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const it = new ITDepartment("01", ["Manu", "Melchior"]);
+it.addEmployee("Xavier");
+it.addEmployee("Robespierre");
+it.printEmployeeInformation();
+console.log(it);
+const accounting = new AccountingDepartment("01", ["r1", "r2"]);
+accounting.addEmployee("Xavier");
+accounting.addEmployee("Robespierre");
+accounting.printEmployeeInformation();
+accounting.addReport("r3");
+accounting.printReports();
+console.log(accounting);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,69 @@
 class Department {
-  name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {}
+
+  //this argument is not required, but tells the compiler that this should point to object of type Department, to prevent possible 'this' errors
+  describe(this: Department) {
+    console.log(`Department ${this.id}: ${this.name}`);
   }
 
-  describe() {
-    console.log(`Department: ${this.name}`);
+  addEmployee(this: Department, employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation(this: Department) {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    // assigns "IT" to department name field of super
+    super(id, "IT");
+  }
+}
 
-accounting.describe();
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    // assigns "IT" to department name field of super
+    super(id, "IT");
+  }
+
+  addReport(this: AccountingDepartment, text: string) {
+    this.reports.push(text);
+  }
+
+  printReports(this: AccountingDepartment) {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("01", ["Manu", "Melchior"]);
+it.addEmployee("Xavier");
+it.addEmployee("Robespierre");
+it.printEmployeeInformation();
+console.log(it);
+
+const accounting = new AccountingDepartment("01", ["r1", "r2"]);
+accounting.addEmployee("Xavier");
+accounting.addEmployee("Robespierre");
+accounting.printEmployeeInformation();
+accounting.addReport("r3");
+accounting.printReports();
+console.log(accounting);
+// creates new object with describe() function inherited from accounting object, with name "accounting" that this will point to
+// const accountingCopy = {
+//   name: "Accounting",
+//   describe: accounting.describe,
+// };
+
+// accountingCopy.describe();
+
+// const humanResources = {
+//   name: "Human Resources",
+//   describe: accounting.describe,
+// };
+
+// humanResources.describe();
