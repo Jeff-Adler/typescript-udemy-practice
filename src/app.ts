c76eb6e5e049e399945c3e2a853c0f80a54df5d6
Jeff Adler <JeffM.Adler@gmail.com>
2021-01-28 19:59:31 -0500

    added ts-node-dev package

--- src/app.ts
+++ src/app.ts
@@ -33,5 +33,5 @@ class Person implements Greetable {
 const user1 = new Person("Jeff", 30);
 user1.greet("Hi there");
 
-const user2 = new Person("John", 30, "Pramp");
-user2.greet("Hello");
+const user2 = new Person("Billy", 30, "Pramp");
+user2.greet("Hel33lo23234");
