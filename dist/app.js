4453daaefb46dcde860923646a4dd855d23c160f
Jeff Adler <JeffM.Adler@gmail.com>
2021-01-30 13:52:28 -0500

    function overloading

--- dist/app.js
+++ dist/app.js
@@ -1,77 +1,16 @@
 "use strict";
-console.log("running2");
+console.log("running");
 const e1 = {
     name: "Max",
     privileges: ["create-server"],
     startDate: new Date(),
 };
-const add = (a, b) => {
+function add(a, b) {
     if (typeof a === "string" || typeof b === "string") {
         return a.toString() + b.toString();
     }
     return a + b;
-};
-const printEmployeeInformation = (emp) => {
-    console.log("Name: " + emp.name);
-    if ("privileges" in emp) {
-        console.log("Privileges: " + emp.privileges);
-    }
-    if ("startDate" in emp) {
-        console.log("Start Date: " + emp.startDate);
-    }
-};
-const ue1 = {
-    name: "Jeff",
-    privileges: ["setup-admin"],
-    startDate: new Date(),
-};
-printEmployeeInformation(ue1);
-class Car {
-    drive() {
-        console.log("Driving...");
-    }
 }
-class Truck {
-    drive() {
-        console.log("Driving a truck...");
-    }
-    loadCargo(amount) {
-        console.log("Loading cargo..." + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-const useVehicle = (vehicle) => {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-};
-useVehicle(v1);
-useVehicle(v2);
-const moveAnimal = (animal) => {
-    let speed;
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log("Moving at speed: " + speed);
-};
-const a1 = {
-    type: "bird",
-    flyingSpeed: 30,
-};
-const a2 = {
-    type: "horse",
-    runningSpeed: 50,
-};
-moveAnimal(a1);
-moveAnimal(a2);
-const userInputElement = (document.getElementById("user-input"));
-const userInputElement2 = document.getElementById("user-input");
-userInputElement.value = "Hi there!";
+const result = add("Jeff", "Adler");
+result.split(" ");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-console.log("running2");
+console.log("running");
 
 type Admin = {
   name: string;
@@ -23,113 +23,132 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
-const add = (a: Combinable, b: Combinable) => {
+//Function overloading specifies the return type based on the types of input parameters
+function add(a: string, b: string): string;
+function add(a: number, b: number): number;
+function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
-};
-
-type UnknownEmployee = Employee | Admin;
-
-const printEmployeeInformation = (emp: UnknownEmployee) => {
-  console.log("Name: " + emp.name);
-  //type check for object property
-  if ("privileges" in emp) {
-    console.log("Privileges: " + emp.privileges);
-  }
-  if ("startDate" in emp) {
-    console.log("Start Date: " + emp.startDate);
-  }
-};
-
-const ue1: UnknownEmployee = {
-  name: "Jeff",
-  privileges: ["setup-admin"],
-  startDate: new Date(),
-};
-
-printEmployeeInformation(ue1);
-
-class Car {
-  drive() {
-    console.log("Driving...");
-  }
 }
 
-class Truck {
-  drive() {
-    console.log("Driving a truck...");
-  }
+const result = add("Jeff", "Adler");
+//Because of overloaded function above, TypeScript allows this. It knows that result will be of type string
+result.split(" ");
 
-  loadCargo(amount: number) {
-    console.log("Loading cargo..." + amount);
-  }
-}
+// type UnknownEmployee = Employee | Admin;
 
-type Vehicle = Car | Truck;
+// const printEmployeeInformation = (emp: UnknownEmployee) => {
+//   console.log("Name: " + emp.name);
+//   //type check for object property
+//   if ("privileges" in emp) {
+//     console.log("Privileges: " + emp.privileges);
+//   }
+//   if ("startDate" in emp) {
+//     console.log("Start Date: " + emp.startDate);
+//   }
+// };
 
-const v1 = new Car();
-const v2 = new Truck();
+// const ue1: UnknownEmployee = {
+//   name: "Jeff",
+//   privileges: ["setup-admin"],
+//   startDate: new Date(),
+// };
 
-const useVehicle = (vehicle: Vehicle) => {
-  vehicle.drive();
-  //type checker for classes
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(1000);
-  }
-};
+// printEmployeeInformation(ue1);
 
-useVehicle(v1);
-useVehicle(v2);
+// class Car {
+//   drive() {
+//     console.log("Driving...");
+//   }
+// }
 
-interface Bird {
-  type: "bird"; //literal type
-  flyingSpeed: number;
-}
+// class Truck {
+//   drive() {
+//     console.log("Driving a truck...");
+//   }
 
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
+//   loadCargo(amount: number) {
+//     console.log("Loading cargo..." + amount);
+//   }
+// }
 
-type Animal = Bird | Horse;
+// type Vehicle = Car | Truck;
 
-const moveAnimal = (animal: Animal) => {
-  let speed;
-  //discriminated union type check, to check for properties not shared by unioned types
-  switch (animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.runningSpeed;
-      break;
-  }
-  console.log("Moving at speed: " + speed);
-};
+// const v1 = new Car();
+// const v2 = new Truck();
 
-const a1: Bird = {
-  type: "bird",
-  flyingSpeed: 30,
-};
+// const useVehicle = (vehicle: Vehicle) => {
+//   vehicle.drive();
+//   //type checker for classes
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(1000);
+//   }
+// };
 
-const a2: Horse = {
-  type: "horse",
-  runningSpeed: 50,
-};
+// useVehicle(v1);
+// useVehicle(v2);
 
-moveAnimal(a1);
-moveAnimal(a2);
+// interface Bird {
+//   type: "bird"; //literal type
+//   flyingSpeed: number;
+// }
 
-//Equivalent:
-const userInputElement = <HTMLInputElement>(
-  document.getElementById("user-input")!
-);
+// interface Horse {
+//   type: "horse";
+//   runningSpeed: number;
+// }
 
-//To:
-const userInputElement2 = document.getElementById(
-  "user-input"
-)! as HTMLInputElement;
+// type Animal = Bird | Horse;
 
-userInputElement.value = "Hi there!";
+// const moveAnimal = (animal: Animal) => {
+//   let speed;
+//   //discriminated union type check, to check for properties not shared by unioned types
+//   switch (animal.type) {
+//     case "bird":
+//       speed = animal.flyingSpeed;
+//       break;
+//     case "horse":
+//       speed = animal.runningSpeed;
+//       break;
+//   }
+//   console.log("Moving at speed: " + speed);
+// };
+
+// const a1: Bird = {
+//   type: "bird",
+//   flyingSpeed: 30,
+// };
+
+// const a2: Horse = {
+//   type: "horse",
+//   runningSpeed: 50,
+// };
+
+// moveAnimal(a1);
+// moveAnimal(a2);
+
+// //Equivalent:
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+
+// //To:
+// const userInputElement2 = document.getElementById(
+//   "user-input"
+// )! as HTMLInputElement;
+
+// userInputElement.value = "Hi there!";
+
+// interface ErrorContainer {
+//   // Should check for errors like { email: 'Not a valid email', username: 'Must start with a character!' }
+
+//   //Index Property: whatever properties you have here, every property must be a string that points to a string
+//   [prop: string]: string;
+// }
+
+// const errorBag: ErrorContainer = {
+//   email: "Not a valid email!",
+//   username: "Must start with a capital character!",
+// };
