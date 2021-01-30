57c9a6f336814bc1ba53e487b306209b0c016014
Jeff Adler <JeffM.Adler@gmail.com>
2021-01-30 13:32:03 -0500

    union type checks

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,74 @@
 "use strict";
+console.log("running2");
+const e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
 const add = (a, b) => {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
     return a + b;
 };
-class Person {
-    constructor(name, age, surname) {
-        this.name = name;
-        this.age = age;
-        this.surname = surname;
+const printEmployeeInformation = (emp) => {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
     }
-    greet(phrase) {
-        const surname = this.surname ? this.surname : "";
-        console.log(`${phrase} ${this.name} ${surname}`);
+    if ("startDate" in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+};
+const ue1 = {
+    name: "Jeff",
+    privileges: ["setup-admin"],
+    startDate: new Date(),
+};
+printEmployeeInformation(ue1);
+class Car {
+    drive() {
+        console.log("Driving...");
     }
 }
-const user1 = new Person("Jeff", 30);
-user1.greet("Hi there");
-const user2 = new Person("John", 30, "Pramp");
-user2.greet("Hello");
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo..." + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+const useVehicle = (vehicle) => {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+};
+useVehicle(v1);
+useVehicle(v2);
+const moveAnimal = (animal) => {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("Moving at speed: " + speed);
+};
+const a1 = {
+    type: "bird",
+    flyingSpeed: 30,
+};
+const a2 = {
+    type: "horse",
+    runningSpeed: 50,
+};
+moveAnimal(a1);
+moveAnimal(a2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+console.log("running2");
+
 type Admin = {
   name: string;
   privileges: string[];
@@ -16,6 +18,106 @@ const e1: ElevatedEmployee = {
   startDate: new Date(),
 };
 
-console.log(e1.name);
-console.log(e1.privileges);
-console.log("test");
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+const add = (a: Combinable, b: Combinable) => {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+};
+
+type UnknownEmployee = Employee | Admin;
+
+const printEmployeeInformation = (emp: UnknownEmployee) => {
+  console.log("Name: " + emp.name);
+  //type check for object property
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+};
+
+const ue1: UnknownEmployee = {
+  name: "Jeff",
+  privileges: ["setup-admin"],
+  startDate: new Date(),
+};
+
+printEmployeeInformation(ue1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive();
+  //type checker for classes
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+};
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: "bird"; //literal type
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const moveAnimal = (animal: Animal) => {
+  let speed;
+  //discriminated union type check, to check for properties not shared by unioned types
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving at speed: " + speed);
+};
+
+const a1: Bird = {
+  type: "bird",
+  flyingSpeed: 30,
+};
+
+const a2: Horse = {
+  type: "horse",
+  runningSpeed: 50,
+};
+
+moveAnimal(a1);
+moveAnimal(a2);
