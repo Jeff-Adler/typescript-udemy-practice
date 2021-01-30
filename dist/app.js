6cd403b3c8fede32b150a98feb0dd4eb1d9372e0
Jeff Adler <JeffM.Adler@gmail.com>
2021-01-30 14:43:22 -0500

    generic types

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,12 @@
 "use strict";
+const names = ["Jeff", "Manuel"];
+const merge = (objA, objB) => {
+    return Object.assign(objA, objB);
+};
+const mergedObj = merge({ name: "Jeff" }, { age: 30 });
+const mergedObj2 = merge({ id: 1 }, { age: 30 });
+const mergedObj3 = merge({ name: "Jeff" }, { vegetarian: false });
+console.log(mergedObj);
+console.log(mergedObj2);
+console.log(mergedObj3);
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,27 @@
+const names: Array<string> = ["Jeff", "Manuel"];
+
+// Promise, like Array, is a generic type. We can specify what specific type of generic with <>
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+
+//Generics T and U say that objA and objB might be different types, but
+// merge() will return the intersection of the types of objA and objB.
+//Optional extends keyword constraints the available types for T and U
+const merge = <T extends object, U extends object>(objA: T, objB: U) => {
+  return Object.assign(objA, objB);
+};
+
+//TS infers the value of mergedObj & mergedObj2 through the generic above
+const mergedObj = merge({ name: "Jeff" }, { age: 30 });
+const mergedObj2 = merge({ id: 1 }, { age: 30 });
+//Explicitly states the type: this is redundant though, and should be avoided
+const mergedObj3 = merge<{ name: string }, { vegetarian: boolean }>(
+  { name: "Jeff" },
+  { vegetarian: false }
+);
+console.log(mergedObj);
+console.log(mergedObj2);
+console.log(mergedObj3);
